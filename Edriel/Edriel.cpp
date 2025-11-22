1e6a8355e011b37eed0fba62b5fe63bd4d6e178a
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2025-11-22 21:28:47 +0900

    init(*): add test for asio, grpc, and protobuf
    initial test

---
+++ Edriel/Edriel.cpp
@@ -0,0 +1,25 @@
+﻿// Edriel.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "Edriel.hpp"
+#include <asio.hpp>
+#include <google/protobuf/stubs/common.h>
+#include <grpcpp/grpcpp.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Asio test
+    asio::io_context io;
+    cout << "Asio io_context created." << endl;
+
+    // Protobuf test
+    cout << "Protobuf version: " << GOOGLE_PROTOBUF_VERSION << endl;
+
+    // gRPC test
+    cout << "gRPC version: " << grpc::Version() << endl;
+
+	return 0;
+}
